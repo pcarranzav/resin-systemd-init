3d5465a98ee24b7f4e1980cea840e44b10849867
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-04-20 21:11:45 +0100

    revert to info log level

--- entry.sh
+++ entry.sh
@@ -6,7 +6,6 @@ echo -e "#!/bin/bash\n exec $@" > /etc/docker.cmd
 chmod +x /etc/docker.cmd
 
 echo 'ForwardToConsole=yes' >> /etc/systemd/journald.conf
-echo 'MaxLevelConsole=err'	>> /etc/systemd/journald.conf
 
 systemctl enable /etc/systemd/system/launch.service
 
