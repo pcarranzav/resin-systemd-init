4dcab8435450306fe6ce6a3510269b825653ecec
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-05-12 13:42:32 -0300

    cleanup entry.sh

--- entry.sh
+++ entry.sh
@@ -8,14 +8,9 @@ if [ "$INITSYSTEM" = "on" ]; then
 	echo -e "#!/bin/bash\n exec $@" > /etc/resinApp.cmd
 	chmod +x /etc/resinApp.cmd
 
-	#echo 'ForwardToConsole=yes' >> /etc/systemd/journald.conf
-
-	#udevadm trigger
-
 	systemctl --quiet enable /etc/systemd/system/launch.service &> /dev/null
 	systemctl --quiet enable /etc/systemd/system/devtmpfs.mount &> /dev/null
 
-	#systemctl enable /etc/systemd/system/udev-trigger.service
 	exec /sbin/init quiet
 else
 	CMD=$(which $1)
