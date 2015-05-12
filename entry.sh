766ee897aa155fbb4d194395831c4ec036a27913
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-05-12 19:00:43 -0300

    Check tty again

--- entry.sh
+++ entry.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 if [ "$INITSYSTEM" = "on" ]; then
+	tty
 	GREEN='\033[0;32m'
 	echo -e "${GREEN}Systemd init system enabled."
 	env > /etc/docker.env
@@ -13,7 +14,7 @@ if [ "$INITSYSTEM" = "on" ]; then
 	echo Hi > /dev/console
 	mount -t devtmpfs none /dev
 	echo Bye > /dev/console
-	
+
 	exec /sbin/init quiet
 else
 	CMD=$(which $1)
