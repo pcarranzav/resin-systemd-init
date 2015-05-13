1489deca44e1a7477ebfd9b869417191ecc7e1d9
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-05-13 11:11:13 -0300

    App outputs to /dev/pts/0

--- entry.sh
+++ entry.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ "$INITSYSTEM" = "on" ]; then
-	tty
 	GREEN='\033[0;32m'
 	echo -e "${GREEN}Systemd init system enabled."
 	env > /etc/docker.env
@@ -11,9 +10,7 @@ if [ "$INITSYSTEM" = "on" ]; then
 
 	systemctl --quiet enable /etc/systemd/system/launch.service &> /dev/null
 
-	echo Hi > /dev/console
 	mount -t devtmpfs none /dev
-	echo Bye > /dev/console
 
 	exec /sbin/init quiet
 else
